543570f27f2de1be3cc62b8f5900a627832c9ee4
Joaquin linares <linaresjoaquin9@gmail.com>
2026-04-19 16:33:25 -0300

    feat: implementacion pwt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Hoy Pinta',
+        short_name: 'HoyPinta',
+        description: 'Qué comemos hoy',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: './public/icono-HoyPinta.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './public/icono-HoyPinta.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
